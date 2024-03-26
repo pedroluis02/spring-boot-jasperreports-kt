3e19a7536274a4f3e3420f1eb4aa1c0d78f5fc57
Pedro Luis <pedroluisnr22@gmail.com>
2024-03-25 21:11:30 -0500

    chore: 🔧 first commit

---
+++ src/main/kotlin/com/github/pedroluis02/springbootsamples/jasperreports/SpringJasperReportsApplication.kt
@@ -0,0 +1,11 @@
+package com.github.pedroluis02.springbootsamples.jasperreports
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringJasperReportsApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringJasperReportsApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/pedroluis02/springbootsamples/jasperreports/SpringJasperReportsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.pedroluis02.springbootsamples.jasperreports
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringJasperReportsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
