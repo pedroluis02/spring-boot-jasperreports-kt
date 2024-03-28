7f43378f3c8cb11a0a82278bc9214471dbc6cf43
Pedro Luis <pedroluisnr22@gmail.com>
2024-03-27 22:02:17 -0500

    build: :package: add dependencies for barcodes Add barcode4j, zxing and xml gracphis

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,12 +20,19 @@ repositories {
 
 dependencies {
     val jasperReportsVersion = "6.21.2"
+    val xmlGraphicsVersion = "1.17"
+    val barcode4jVersion = "2.1"
+    val zxingVersion = "3.5.3"
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("net.sf.jasperreports:jasperreports:$jasperReportsVersion")
+
+    runtimeOnly("org.apache.xmlgraphics:batik-bridge:$xmlGraphicsVersion")
+    runtimeOnly("net.sf.barcode4j:barcode4j:$barcode4jVersion")
+    runtimeOnly("com.google.zxing:javase:$zxingVersion")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
