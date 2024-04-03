029f8b8720a2cbcab35db570ec4464b4b067e323
Pedro Luis <pedroluisnr22@gmail.com>
2024-04-03 01:22:09 -0500

    test: :white_check_mark: test summary inventory

--- src/test/kotlin/com/github/pedroluis02/springbootsamples/jasperreports/JasperInventoryReportTest.kt
+++ src/test/kotlin/com/github/pedroluis02/springbootsamples/jasperreports/JasperInventoryReportTest.kt
@@ -2,12 +2,15 @@ package com.github.pedroluis02.springbootsamples.jasperreports
 
 import com.github.pedroluis02.springbootsamples.jasperreports.model.inventory.Inventory
 import com.github.pedroluis02.springbootsamples.jasperreports.model.inventory.InventoryHeader
+import com.github.pedroluis02.springbootsamples.jasperreports.model.inventory.InventorySummary
 import com.github.pedroluis02.springbootsamples.jasperreports.service.JasperInventoryReportService
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.io.File
 import java.util.*
+import java.util.stream.Collectors
+import kotlin.random.Random
 
 class JasperInventoryReportTest {
 
@@ -29,9 +32,22 @@ class JasperInventoryReportTest {
 
     private fun createInventory(): Inventory {
         val id = UUID.randomUUID().toString()
-        print(id)
         val header = InventoryHeader(id, "General report", "Administrator")
 
-        return Inventory(header)
+        return Inventory(header, createSummary())
+    }
+
+    private fun createSummary(): List<InventorySummary> {
+        val random = Random(20)
+        return (1..12).map {
+            InventorySummary(
+                it,
+                "code-$it",
+                "Company Name $it",
+                random.nextInt(),
+                random.nextInt(),
+                random.nextInt()
+            )
+        }.stream().collect(Collectors.toList())
     }
 }
