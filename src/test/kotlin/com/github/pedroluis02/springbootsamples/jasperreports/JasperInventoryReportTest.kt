7355cd8ae267394a203d43eba4a8d6058db961e7
Pedro Luis <pedroluisnr22@gmail.com>
2024-04-03 21:29:58 -0500

    test: :white_check_mark: test device table of template

--- src/test/kotlin/com/github/pedroluis02/springbootsamples/jasperreports/JasperInventoryReportTest.kt
+++ src/test/kotlin/com/github/pedroluis02/springbootsamples/jasperreports/JasperInventoryReportTest.kt
@@ -1,5 +1,6 @@
 package com.github.pedroluis02.springbootsamples.jasperreports
 
+import com.github.pedroluis02.springbootsamples.jasperreports.model.Device
 import com.github.pedroluis02.springbootsamples.jasperreports.model.inventory.Inventory
 import com.github.pedroluis02.springbootsamples.jasperreports.model.inventory.InventoryHeader
 import com.github.pedroluis02.springbootsamples.jasperreports.model.inventory.InventorySummary
@@ -32,21 +33,36 @@ class JasperInventoryReportTest {
 
     private fun createInventory(): Inventory {
         val id = UUID.randomUUID().toString()
-        val header = InventoryHeader(id, "General report", "Administrator")
+        val header = InventoryHeader(id, "General report of devices", "Administrator")
 
-        return Inventory(header, createSummary())
+        return Inventory(header, createSummary(), createDevices())
     }
 
     private fun createSummary(): List<InventorySummary> {
         val random = Random(20)
-        return (1..12).map {
+        return (1..10).map {
             InventorySummary(
                 it,
-                "code-$it",
+                "CODE-$it",
                 "Company Name $it",
                 random.nextInt(),
                 random.nextInt(),
                 random.nextInt()
+            )
+        }.stream().collect(Collectors.toList())
+    }
+
+    private fun createDevices(): List<Device> {
+        return (1..20).map {
+            Device(
+                it,
+                "TYPE-$it",
+                "CODE-$it",
+                "BRAND-$it",
+                "MODEL-$it",
+                Date(),
+                "Responsible-$it",
+                "ACTIVE"
             )
         }.stream().collect(Collectors.toList())
     }
