9db58cd4aa8848d723b40a7966958aec3cacbb8d
Pedro Luis <pedroluisnr22@gmail.com>
2024-06-13 20:47:42 -0500

    test: :hammer: use inventory data sample

--- src/test/kotlin/com/github/pedroluis02/springbootsamples/jasperreports/InventoryReportGeneratorTest.kt
+++ src/test/kotlin/com/github/pedroluis02/springbootsamples/jasperreports/InventoryReportGeneratorTest.kt
@@ -1,17 +1,11 @@
 package com.github.pedroluis02.springbootsamples.jasperreports
 
-import com.github.pedroluis02.springbootsamples.jasperreports.model.Device
-import com.github.pedroluis02.springbootsamples.jasperreports.model.inventory.Inventory
-import com.github.pedroluis02.springbootsamples.jasperreports.model.inventory.InventoryHeader
-import com.github.pedroluis02.springbootsamples.jasperreports.model.inventory.InventorySummary
+import com.github.pedroluis02.springbootsamples.jasperreports.sample.createInventorySample
 import com.github.pedroluis02.springbootsamples.jasperreports.service.InventoryReportGeneratorService
 import com.github.pedroluis02.springbootsamples.jasperreports.service.InventoryReportGeneratorServiceImpl
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import java.util.*
-import java.util.stream.Collectors
-import kotlin.random.Random
 
 class InventoryReportGeneratorTest {
 
@@ -24,46 +18,12 @@ class InventoryReportGeneratorTest {
 
     @Test
     fun shouldGeneratePdf() {
-        val data = createInventory()
-        val output = System.currentTimeMillis().toString() + ".pdf"
+        val data = createInventorySample()
+        val output = "test-" + System.currentTimeMillis().toString() + ".pdf"
 
         val file = service.generatePdf(data, output, false)
         assertThat(file).exists()
-    }
 
-    private fun createInventory(): Inventory {
-        val id = UUID.randomUUID().toString()
-        val header = InventoryHeader(id, "General report of devices", "Administrator")
-
-        return Inventory(header, createSummary(), createDevices())
-    }
-
-    private fun createSummary(): List<InventorySummary> {
-        val random = Random(20)
-        return (1..10).map {
-            InventorySummary(
-                it,
-                "CODE-$it",
-                "Company Name $it",
-                random.nextInt(),
-                random.nextInt(),
-                random.nextInt()
-            )
-        }.stream().collect(Collectors.toList())
-    }
-
-    private fun createDevices(): List<Device> {
-        return (1..20).map {
-            Device(
-                it,
-                "TYPE-$it",
-                "CODE-$it",
-                "BRAND-$it",
-                "MODEL-$it",
-                Date(),
-                "Responsible-$it",
-                "ACTIVE"
-            )
-        }.stream().collect(Collectors.toList())
+        assertThat(file.delete()).isTrue()
     }
 }
