5663aef6d5661ed657709e0cdaaef95613578418
Pedro Luis <pedroluisnr22@gmail.com>
2024-06-13 20:48:31 -0500

    feat: :sparkles: add console application for report generation

--- src/main/kotlin/com/github/pedroluis02/springbootsamples/jasperreports/SpringJasperReportsApplication.kt
+++ src/main/kotlin/com/github/pedroluis02/springbootsamples/jasperreports/SpringJasperReportsApplication.kt
@@ -1,10 +1,24 @@
 package com.github.pedroluis02.springbootsamples.jasperreports
 
+import com.github.pedroluis02.springbootsamples.jasperreports.sample.createInventorySample
+import com.github.pedroluis02.springbootsamples.jasperreports.service.InventoryReportGeneratorService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SpringJasperReportsApplication
+class SpringJasperReportsApplication : CommandLineRunner {
+
+    @Autowired
+    private lateinit var service: InventoryReportGeneratorService
+
+    override fun run(vararg args: String?) {
+        val data = createInventorySample()
+        val output = "cmd-" + System.currentTimeMillis().toString() + ".pdf"
+        service.generatePdf(data, output, false)
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<SpringJasperReportsApplication>(*args)
